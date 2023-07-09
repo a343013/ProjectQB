4d46bbeb890c55e601dad8dac89fc143aad53d81
a343013 <135076362+a343013@users.noreply.github.com>
2023-07-08 23:02:17 -0600

    Update settings.py

--- QualityBeacon/settings.py
+++ QualityBeacon/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/ProjectQB/evaluation/static/'
+STATIC_ROOT = '/evaluation/static/'
 
 
 # Default primary key field type
