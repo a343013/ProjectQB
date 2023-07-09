c87fcca4cbd6d94f3d909c805c3bcff26e75818c
Luis Acosta <a343013@uach.mx>
2023-07-09 13:36:42 -0600

    Agregado actualizar python en build.sh

--- build.sh
+++ build.sh
@@ -3,18 +3,10 @@
 set -o errexit  # exit on error
 
 pip install --upgrade pip
-# Instalar la versión deseada de Python
-pyenv install 3.10.6
-
-# Establecer la versión de Python como global
-pyenv global 3.10.6
 
 poetry install
 poetry self update
 poetry lock
-
-# Continuar con otros comandos de construcción
-
 
 pip install -r requirements.txt
 
